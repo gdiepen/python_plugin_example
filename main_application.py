2e52810483f1dc3831264aa02d19640e1b679bb2
Guido Diepen <site-github@guidodiepen.nl>
2019-02-15 10:13:46 +0100

    Added docstrings

--- main_application.py
+++ main_application.py
@@ -1,11 +1,14 @@
+"""Main applicatoin that demonstrates the functionality of
+the dynamic plugins and the PluginCollection class
+"""
+
 from plugin_collection import PluginCollection
 
-
 def main():
+    """main function that runs the application
+    """
     my_plugins = PluginCollection('plugins')
     my_plugins.apply_all_plugins_on_value(5)
-
-
 
 if __name__ == '__main__':
     main()
