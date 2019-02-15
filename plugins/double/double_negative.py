f70a9056346e4a463e3e9e04e30880ad5626a83b
Guido Diepen <site-github@guidodiepen.nl>
2019-02-15 08:24:26 +0100

    Added two sample plugins
    One plugin will double the argument supplied, while the second one will
    double and negate the supplied argument.

---
+++ plugins/double/double_negative.py
@@ -0,0 +1,11 @@
+from plugin_collection import Plugin
+
+class DoubleNegative:
+    """This plugin will just multiply the argument with the value -2
+    """
+
+    def perform_operation(self, argument):
+        """The actual implementation of this plugin is to multiple the
+        value of the supplied argument by -2
+        """
+        return argument*-22
