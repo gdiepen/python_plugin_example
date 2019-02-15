034914eaba59d60aef5fbe4e5008633ba635066a
Guido Diepen <site-github@guidodiepen.nl>
2019-02-15 09:55:07 +0100

    Updated the double-positive and double-negative plugins

--- plugins/double/double_negative.py
+++ plugins/double/double_negative.py
@@ -1,11 +1,14 @@
-from plugin_collection import Plugin
+import plugin_collection
 
-class DoubleNegative:
+class DoubleNegative(plugin_collection.Plugin):
     """This plugin will just multiply the argument with the value -2
     """
+    def __init__(self):
+        super().__init__()
+        self.description = 'Negative double function'
 
     def perform_operation(self, argument):
         """The actual implementation of this plugin is to multiple the
         value of the supplied argument by -2
         """
-        return argument*-22
+        return argument*-2
--- plugins/double/double_positive.py
+++ plugins/double/double_positive.py
@@ -1,8 +1,11 @@
-from plugin_collection import Plugin
+import plugin_collection
 
-class DoublePositive:
+class DoublePositive(plugin_collection.Plugin):
     """This plugin will just multiply the argument with the value 2
     """
+    def __init__(self):
+        super().__init__()
+        self.description = 'Double function'
 
     def perform_operation(self, argument):
         """The actual implementation of this plugin is to multiple the
